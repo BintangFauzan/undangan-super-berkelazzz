20962999e4bba0c4d198facab4f03d63318e270e
Bintang <bintangfauzan1000@gmail.com>
2025-01-17 15:30:36 +0700

    penyesuaian undangan 1

--- README.md
+++ README.md
@@ -8,19 +8,18 @@
 [![GitHub License](https://img.shields.io/github/license/dewanakl/undangan?color=brightgreen)](https://shields.io)
 
 ## Demo
-Untuk kamu yang ingin melihat demo terlebih dahulu:
+Kamu dapat mencoba undangan ini melalui link berikut.
 
-[https://ulems.my.id](https://ulems.my.id/?to=Teman%20teman%20semua)
+[https://ulems.my.id/?to=Teman%20teman%20semua](https://ulems.my.id/?to=Teman%20teman%20semua)
 
 ## Documentation
 
-- Ubah isi dari index.html sesuai kebutuhan.
-- Jika tidak ingin menggunakan `komentar` bisa hapus `data-url` dan `data-key` pada body index.
-- Sesuaikan `data-url` pada body index dan dashboard sesuai domain backend (jika deploy sendiri).
-- Sesuaikan juga `data-key` dengan access key pada dashboard.
-- Self deployment backend bisa lihat dibawah ini atau ingin coba gratis selama 1 hari pada trial api.
+- Sesuaikan index.html sesuai kebutuhan.
+- Ubah `data-url` pada body index dan dashboard sesuai domain backend (jika deploy sendiri).
+- Ubah juga `data-key` pada body index sesuai dengan access key pada dashboard.
+- Deployment backend bisa lihat dibawah ini atau coba gratis selama 1 hari pada trial api.
 
-> Jika kamu punya pertanyaan, silakan gunakan fitur `discussions` agar teman-teman yang lain juga dapat mengetahuinya.
+Jika kamu punya pertanyaan, silakan gunakan fitur `discussions` agar teman-teman yang lain juga dapat mengetahuinya. Terima kasih. 
 
 ## Deployment API
 
@@ -28,12 +27,12 @@ Untuk kamu yang ingin melihat demo terlebih dahulu:
     otw
 
 - Presentation
-    [https://docs.google.com/presentation](https://docs.google.com/presentation/d/1EY2YmWdZUI7ASoo0f2wvU7ec_Yt0uZanYa8YLbfNysk/edit)
+    [https://docs.google.com/presentation/d/1EY2YmWdZUI7ASoo0f2wvU7ec_Yt0uZanYa8YLbfNysk/edit](https://docs.google.com/presentation/d/1EY2YmWdZUI7ASoo0f2wvU7ec_Yt0uZanYa8YLbfNysk/edit)
 
 ## Trial API
-Untuk kamu yang ingin mencoba secara gratis:
+Untuk kamu yang ingin mencoba secara gratis, silakan kunjungi link berikut.
 
-[https://trial.ulems.my.id](https://trial.ulems.my.id)
+[https://payment.ulems.my.id](https://payment.ulems.my.id)
 
 
 ## Tech stack
--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -10,274 +10,227 @@ import { bootstrap } from '../../libs/bootstrap.js';
 import { confetti, openAnimation } from '../../libs/confetti.js';
 
 export const guest = (() => {
+  /**
+   * @type {ReturnType<typeof storage>|null}
+   */
+  let information = null;
 
-    /**
-     * @type {ReturnType<typeof storage>|null}
-     */
-    let information = null;
+  /**
+   * @returns {void}
+   */
+  const countDownDate = () => {
+    const until = document.getElementById('count-down')?.getAttribute('data-time')?.replace(' ', 'T');
+    if (!until) {
+      return;
+    }
 
-    /**
-     * @returns {void}
-     */
-    const countDownDate = () => {
-        const until = document.getElementById('count-down')?.getAttribute('data-time')?.replace(' ', 'T');
-        if (!until) {
-            return;
-        }
+    const count = new Date(until).getTime();
 
-        const count = (new Date(until)).getTime();
+    setInterval(() => {
+      const distance = Math.abs(count - Date.now());
 
-        setInterval(() => {
-            const distance = Math.abs(count - Date.now());
+      document.getElementById('day').innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
+      document.getElementById('hour').innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      document.getElementById('minute').innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      document.getElementById('second').innerText = Math.floor((distance % (1000 * 60)) / 1000);
+    }, 1000);
+  };
 
-            document.getElementById('day').innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
-            document.getElementById('hour').innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            document.getElementById('minute').innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            document.getElementById('second').innerText = Math.floor((distance % (1000 * 60)) / 1000);
-        }, 1000);
+  /**
+   * @param {string} id
+   * @param {number} speed
+   * @returns {void}
+   */
+  const opacity = (id, speed = 0.01) => {
+    const el = document.getElementById(id);
+    let op = parseFloat(el.style.opacity);
+
+    let clear = null;
+    const callback = () => {
+      if (op > 0) {
+        el.style.opacity = op.toFixed(3);
+        op -= speed;
+        return;
+      }
+
+      clearInterval(clear);
+      clear = null;
+      el.remove();
     };
 
+    clear = setInterval(callback, 10);
+  };
+
+  /**
+   * @returns {void}
+   */
+  const showGuestName = () => {
     /**
-     * @param {string} id
-     * @param {number} speed
-     * @returns {void}
+     * Make sure "to=" is the last query string.
+     * Ex. domain.my.id/?id=some-uuid-here&to=name
      */
-    const opacity = (id, speed = 0.01) => {
-        const el = document.getElementById(id);
-        let op = parseFloat(el.style.opacity);
+    const raw = window.location.search.split('to=');
+    let name = null;
 
-        let clear = null;
-        const callback = () => {
-            if (op > 0) {
-                el.style.opacity = op.toFixed(3);
-                op -= speed;
-                return;
-            }
+    if (raw.length > 1 && raw[1].length > 0) {
+      name = window.decodeURIComponent(raw[1]);
+    }
 
-            clearInterval(clear);
-            clear = null;
-            el.remove();
-        };
-
-        clear = setInterval(callback, 10);
-    };
-
-    /**
-     * @returns {void}
-     */
-    const showGuestName = () => {
-        /**
-         * Make sure "to=" is the last query string.
-         * Ex. domain.my.id/?id=some-uuid-here&to=name
-         */
-        const raw = window.location.search.split('to=');
-        let name = null;
-
-        if (raw.length > 1 && raw[1].length > 0) {
-            name = window.decodeURIComponent(raw[1]);
-        }
-
-        if (name) {
-            const guest = document.getElementById('guest-name');
-            const div = document.createElement('div');
-            div.classList.add('m-2');
-            div.innerHTML = `
+    if (name) {
+      const guest = document.getElementById('guest-name');
+      const div = document.createElement('div');
+      div.classList.add('m-2');
+      div.innerHTML = `
                 <p class="mt-0 mb-1 mx-0 p-0" style="font-size: 0.95rem;">${guest?.getAttribute('data-message')}</p>
                 <h2 class="m-0 p-0">${util.escapeHtml(name)}</h2>`;
 
-            guest?.appendChild(div);
-        }
+      guest?.appendChild(div);
+    }
 
-        const form = document.getElementById('form-name');
-        if (form) {
-            form.value = information.get('name') ?? name;
-        }
+    const form = document.getElementById('form-name');
+    if (form) {
+      form.value = information.get('name') ?? name;
+    }
 
-        // remove loading screen
-        opacity('loading', 0.025);
-    };
+    // remove loading screen
+    opacity('loading', 0.025);
+  };
 
-    /**
-     * @param {HTMLButtonElement} button
-     * @returns {void}
-     */
-    const open = (button) => {
-        button.disabled = true;
-        document.body.scrollIntoView({ behavior: 'instant' });
+  /**
+   * @param {HTMLButtonElement} button
+   * @returns {void}
+   */
+  const open = (button) => {
+    button.disabled = true;
+    document.body.scrollIntoView({ behavior: 'instant' });
 
-        if (!theme.isAutoMode()) {
-            document.getElementById('button-theme').style.display = 'none';
-        }
+    if (!theme.isAutoMode()) {
+      document.getElementById('button-theme').style.display = 'none';
+    }
 
-        confetti({
-            origin: { y: 1 },
-            zIndex: 1057
-        });
+    confetti({
+      origin: { y: 1 },
+      zIndex: 1057,
+    });
 
-        opacity('welcome', 0.025);
+    opacity('welcome', 0.025);
 
-        audio.init();
-        theme.spyTop();
+    audio.init();
+    theme.spyTop();
 
-        util.timeOut(openAnimation, 1500);
-    };
+    util.timeOut(openAnimation, 1500);
+  };
 
-    /**
-     * @param {HTMLImageElement} img
-     * @returns {void}
-     */
-    const modal = (img) => {
-        document.getElementById('show-modal-image').src = img.src;
-        bootstrap.Modal.getOrCreateInstance('#modal-image').show();
-    };
+  /**
+   * @param {HTMLImageElement} img
+   * @returns {void}
+   */
+  const modal = (img) => {
+    document.getElementById('show-modal-image').src = img.src;
+    bootstrap.Modal.getOrCreateInstance('#modal-image').show();
+  };
 
-    /**
-     * @returns {void}
-     */
-    const closeInformation = () => information.set('info', true);
+  /**
+   * @returns {void}
+   */
+  const closeInformation = () => information.set('info', true);
 
-    /**
-     * @param {HTMLElement} svg
-     * @param {number} timeout
-     * @param {string} classes
-     * @returns {void}
-     */
-    const animate = (svg, timeout, classes) => util.timeOut(() => svg.classList.add(classes), timeout);
+  /**
+   * @param {HTMLElement} svg
+   * @param {number} timeout
+   * @param {string} classes
+   * @returns {void}
+   */
+  const animate = (svg, timeout, classes) => util.timeOut(() => svg.classList.add(classes), timeout);
 
-    /**
-     * @returns {void}
-     */
-    const normalize = () => {
-        document.querySelectorAll('.font-arabic').forEach((el) => {
-            el.innerHTML = String(el.innerHTML).normalize('NFC');
-        });
-    };
+  /**
+   * @returns {void}
+   */
+  const normalize = () => {
+    document.querySelectorAll('.font-arabic').forEach((el) => {
+      el.innerHTML = String(el.innerHTML).normalize('NFC');
+    });
+  };
 
-    /**
-     * @returns {void}
-     */
-    const imageProgress = () => {
-        /**
-         * @param {HTMLImageElement} el 
-         * @returns {void}
-         */
-        const getByFetch = (el) => {
-            fetch(el.getAttribute('data-src'))
-                .then((res) => res.blob())
-                .then((b) => {
-                    el.src = URL.createObjectURL(b);
-                    progress.complete('image');
-                })
-                .catch(() => progress.invalid('image'));
-        };
+  /**
+   * @returns {object}
+   */
+  const init = () => {
+    theme.init();
+    session.init();
+    offline.init();
+    window.AOS.init();
 
-        /**
-         * @param {HTMLImageElement} el 
-         * @returns {void}
-         */
-        const getByDefault = (el) => {
-            el.onerror = () => progress.invalid('image');
-            el.onload = () => progress.complete('image');
+    normalize();
+    countDownDate();
+    information = storage('information');
+    document.addEventListener('progressDone', showGuestName);
 
-            if (el.complete && el.naturalWidth !== 0 && el.naturalHeight !== 0) {
-                progress.complete('image');
-            } else if (el.complete) {
-                progress.invalid('image');
-            }
-        };
+    if (session.isAdmin()) {
+      storage('user').clear();
+      storage('owns').clear();
+      storage('likes').clear();
+      storage('session').clear();
+      storage('comment').clear();
+      storage('tracker').clear();
+    }
 
-        document.querySelectorAll('img').forEach((el) => el.hasAttribute('data-src') ? getByFetch(el) : getByDefault(el));
-    };
+    if (information.has('presence')) {
+      document.getElementById('form-presence').value = information.get('presence') ? '1' : '2';
+    }
 
-    /**
-     * @returns {object}
-     */
-    const init = () => {
-        theme.init();
-        session.init();
-        offline.init();
-        progress.init();
-        window.AOS.init();
+    const info = document.getElementById('information');
+    if (info && information.get('info')) {
+      info.remove();
+    }
 
-        normalize();
-        countDownDate();
-        information = storage('information');
-        document.addEventListener('progressDone', showGuestName);
+    const token = document.body.getAttribute('data-key');
+    if (!token || token.length === 0) {
+      progress.init();
+      document.getElementById('comment')?.remove();
+      document.querySelector('a.nav-link[href="#comment"]')?.closest('li.nav-item')?.remove();
+    }
 
-        if (session.isAdmin()) {
-            storage('user').clear();
-            storage('owns').clear();
-            storage('likes').clear();
-            storage('session').clear();
-            storage('comment').clear();
-            storage('tracker').clear();
-        }
+    if (token.length > 0) {
+      // add 2 progress for config and comment.
+      progress.add();
+      progress.add();
+      progress.init();
 
-        if (information.has('presence')) {
-            document.getElementById('form-presence').value = information.get('presence') ? '1' : '2';
-        }
+      session.setToken(token);
+      session
+        .guest()
+        .then((res) => {
+          if (res.code !== 200) {
+            progress.invalid('config');
+            return;
+          }
 
-        const info = document.getElementById('information');
-        if (info && information.get('info')) {
-            info.remove();
-        }
+          progress.complete('config');
 
-        // add total image.
-        document.querySelectorAll('img').forEach(progress.add);
-
-        const token = document.body.getAttribute('data-key');
-        if (!token || token.length === 0) {
-            imageProgress();
-            document.getElementById('comment')?.remove();
-            document.querySelector('a.nav-link[href="#comment"]')?.closest('li.nav-item')?.remove();
-        }
-
-        if (token.length > 0) {
-            // add 2 progress for config and comment.
-            progress.add();
-            progress.add();
-
-            const hasDataSrc = Array.from(document.querySelectorAll('img')).some((i) => i.hasAttribute('data-src'));
-            if (!hasDataSrc) {
-                imageProgress();
-            }
-
-            session.setToken(token);
-            session.guest()
-                .then((res) => {
-                    if (res.code !== 200) {
-                        progress.invalid('config');
-                        return;
-                    }
-
-                    progress.complete('config');
-
-                    if (hasDataSrc) {
-                        imageProgress();
-                    }
-
-                    comment.init();
-                    comment.comment()
-                        .then(() => progress.complete('comment'))
-                        .catch(() => progress.invalid('comment'));
-                })
-                .catch(() => progress.invalid('config'));
-        }
-
-        return {
-            util,
-            theme,
-            comment,
-            guest: {
-                open,
-                modal,
-                animate,
-                closeInformation,
-            },
-        };
-    };
+          comment.init();
+          comment
+            .comment()
+            .then(() => progress.complete('comment'))
+            .catch(() => progress.invalid('comment'));
+        })
+        .catch(() => progress.invalid('config'));
+    }
 
     return {
-        init,
+      util,
+      theme,
+      comment,
+      guest: {
+        open,
+        modal,
+        animate,
+        closeInformation,
+      },
     };
+  };
+
+  return {
+    init,
+  };
 })();
